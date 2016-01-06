57a5b27c97e14d8a0447dfad00afb8f6c59515b9
IbnuAmin <mihfazhillah@gmail.com>
2016-01-06 14:14:09 +0700

    Create scan_book.py

---
+++ scan_book.py
@@ -0,0 +1,45 @@
+# masukkan direktori
+directory = raw_input("Tulis Directory yang akan kamu pakai: ")
+# tentukan ukuran, x mendatar, y vertikal dalam mm
+try:
+    x = raw_input("tentukan ukuran x(mendatar) dalam mm: ")
+    int(x)
+except ValueError:
+    x = raw_input("tentukan ukuran x(mendatar) dalam mm [harus angka]: ")
+try:
+    y = raw_input("tentukan ukuran y(meninggi) dalam mm: ")
+    int(x)
+except ValueError:
+    y= raw_input("tentukan ukuran y(meninggi) dalam mm [harus angka]: ")
+# tentukan format yang mau dipakai pnm/tiff
+format_pilihan = None
+while format_pilihan is None:
+    format_pilihan = raw_input("pnm/tiff: ")
+    while format_pilihan not in ["pnm", "tiff"]:
+        format_pilihan = raw_input("pnm/tiff (format yang anda pilih salah):")
+x = int(x)
+y = int(y)
+# kemudian, loop dipause dan kalau mau jalankan lagi tinggal enter terminal.
+# otomatis nanti dia buat file dengan nomor yang langsung loop
+# selesai dengan menggukan control c 
+import os
+count = 1
+os.mkdir(directory)
+while True:
+    try:
+        
+        os.system(" ".join([
+                   "sudo",
+                   "scanimage",
+                   "-d brother4",
+                   "-x %d" %x,
+                   "-y %d" %y,
+                   "--format=%s" %format_pilihan,
+                   ">",
+                   directory+"/scanned"+str(count)]))
+        raw_input("enter to continue and ctrl c to exit")
+        count += 1
+    except KeyboardInterrupt:
+        import os
+        print "\nPekerjaan selesai"
+        exit()
